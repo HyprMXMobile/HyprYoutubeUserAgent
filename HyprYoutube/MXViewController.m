f400907a499a63e450b4e14d2b936759d319dadb
Jeremy Ellison <jeremy@twotoasters.com>
2013-06-28 13:19:16 -0400

    Switch view over to HTTPS

--- HyprYoutube/MXViewController.m
+++ HyprYoutube/MXViewController.m
@@ -27,7 +27,7 @@
     NSTimeInterval negative_eighteen_years_in_seconds = -1 * 60 * 60 * 24 * 365 * 18;
     NSDate *date = [NSDate dateWithTimeIntervalSinceNow:negative_eighteen_years_in_seconds];
     
-    NSString *string = [NSString stringWithFormat:@"http://live.hyprmx.com/embedded_videos/catalog_frame?uid=%@&distributorid=7172365&offer=video-0369253220a291d9b28345ef4b05aa09&dob=%@", [[NSProcessInfo processInfo] globallyUniqueString], [formatter stringFromDate:date]];
+    NSString *string = [NSString stringWithFormat:@"https://live.hyprmx.com/embedded_videos/catalog_frame?uid=%@&distributorid=7172365&offer=video-0369253220a291d9b28345ef4b05aa09&dob=%@", [[NSProcessInfo processInfo] globallyUniqueString], [formatter stringFromDate:date]];
     
     NSLog(@"Loading: %@", string);
     
